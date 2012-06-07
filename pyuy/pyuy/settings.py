101bc3833c467cb9a85f3dab388143e51abf6f23
eveiga <ed.cinemascop@yahoo.es>
2012-06-07 15:51:23 -0300

    enabled admin

--- pyuy/pyuy/settings.py
+++ pyuy/pyuy/settings.py
@@ -118,12 +118,10 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admin',
 
     'symposion',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+
 )
 
 ACCEPTING_PROPOSALS = True
