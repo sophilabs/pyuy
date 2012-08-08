3c12e2ec71263c5e6d7df56fc70e08eedaece876
Santiago Paragarino <sparagarino@sophilabs.com>
2012-08-08 15:49:29 -0300

    Set background and google-static-map in every page

--- pyuy/pyuy/models.py
+++ pyuy/pyuy/models.py
@@ -3,18 +3,17 @@ from django.db import models
 class Background(models.Model):
 
     name = models.CharField(max_length=30)
+    autor = models.CharField(max_length=30)
     source = models.URLField()
     image = models.ImageField(upload_to="images")
-    latitude = models.IntegerField()
-    longitude = models.IntegerField()
-    map_source = models.URLField(editable=False)
+    latitude = models.DecimalField(decimal_places=6, max_digits=8)
+    longitude = models.DecimalField(decimal_places=6, max_digits=8)
+    googlemap_url = models.URLField(editable=False)
 
-    def get_map_source(self):
-        url_left = "http://maps.google.com/maps/api/staticmap?center="
-        url_right = "&zoom=15&size=300x300&maptype=roadmap&sensor=false"
-        url_middle = self.latitude+","+self.longitude
-        url = url_left+url_middle+url_right
+    def get_googlemap_url(self):
+        url = "http://maps.google.com/maps/api/staticmap?center=%s,%s&zoom=15&size=300x300&maptype=roadmap&sensor=false" % (self.latitude, self.longitude)
         return url
+    get_googlemap_url.alters_data = False
 
     def __unicode__(self):
         return self.name
