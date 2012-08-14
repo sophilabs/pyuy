0ee1359e02d2f86aa1c304d74d13c127304629c3
Santiago Paragarino <sparagarino@sophilabs.com>
2012-08-14 15:40:23 -0300

    Background model

--- pyuy/background/models.py
+++ pyuy/background/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from pyuy.settings import LANGUAGES
 
 class Background(models.Model):
-    name = models.CharField(max_length=30)
+    title = models.CharField(max_length=40)
+    description = models.TextField()
+    place = models.CharField(max_length=20)
     autor = models.CharField(max_length=30)
+    language = models.CharField(max_length=2, choices=LANGUAGES)
+    order = models.IntegerField(default=0)
     source = models.URLField()
     image = models.ImageField(upload_to="backgrounds")
     latitude = models.DecimalField(decimal_places=6, max_digits=8)
